74e4ccbb4a3bf01a4e9ffca57bb8da9c8861dbc3
Andrew Brusso <a.brusso@techsmith.com>
2023-10-04 15:41:09 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Serilog;
+Log.Logger = new LoggerConfiguration()
+                            // add console as logging target
+                            .WriteTo.Console()
+                            // set default minimum level
+                            .MinimumLevel.Debug()
+                            .CreateLogger();
+Log.Information( "Test Log" );
